b14eaa5dcb2d2c306877e642dbf8992d219a9d3d
sangwook <sangwookp9591@gmail.com>
2022-07-11 21:34:00 +0900

    first commit

---
+++ src/main/java/dev/greeter/GreetingConfig.java
@@ -0,0 +1,9 @@
+package dev.greeter;
+
+import java.util.Properties;
+
+public class GreetingConfig extends Properties {
+
+    private static final long serialVersionUID = 5662570853707247891L;
+
+}
